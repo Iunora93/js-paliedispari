26cf50ade2f3db591a9d71ccd018d29353f3ae29
Iunora93 <eleonoraruggieri22@gmail.com>
2022-10-28 17:30:58 +0200

    Function somma pari o dispari

--- Pari e Dispari/main.js
+++ Pari e Dispari/main.js
@@ -5,15 +5,31 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto. */
 
+/* Chiedo all'utente di inserire un numero */
 const userNumber = Number(prompt("Inserisci un numero da 1 a 5"));
 console.log(userNumber);
 
+/* Genero con una funzione un numero random da 0 a 5 */
 function cpuNumber(max) {
     return Math.floor(Math.random() * max);
   }
-  console.log(cpuNumber(5));
-
+  /* Dichiaro al numero random il max e lo stampo in console */
   const cpuNum = cpuNumber(5);
   console.log(cpuNum);
-  cpuNum = userNumber + cpuNumber;
-  console.log(cpuNum);
+
+  /* Sommo il risultato dei due numeri */
+  let sum = cpuNum + userNumber;
+  console.log(sum);
+
+element = 0;
+let pariDispari = sum;
+
+/* Stabiliamo se la somma è pari o dispari */
+function pariDispari( element ){
+    if (element % 2 === 0 ) {
+        return "pari";
+    }else{
+        return "dispari";
+    }    
+}
+element.alert("il numero è pari");
