7a2d27c2c983dcd481a67dd5c3b10acf12d6b938
Iunora93 <eleonoraruggieri22@gmail.com>
2022-10-28 16:24:35 +0200

    math.random cpu

--- Pari e Dispari/main.js
+++ Pari e Dispari/main.js
@@ -1,1 +1,25 @@
 `use strict`
+/* L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto. */
+
+const userNumber = Number(prompt("Inserisci un numero da 1 a 5"));
+console.log(userNumber);
+
+function cpuNumber(min, max) {
+    function Math.floor(Math.random() * (max - min + 1)) + min;    
+}
+console.log(cpuNumber);
+
+
+/* let = 0;
+while (i < userNumber.length);{
+    console.log(userNumber);
+    userNumber = userNumber.length[i];
+
+    i++;
+} */
+
+
